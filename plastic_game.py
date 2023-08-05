92969ee50d18b6d05fac8b1a2604f36b169fd993
t0mwelsh <139456825+t0mwelsh@users.noreply.github.com>
2023-08-05 21:07:42 +0100

    Update plastic_game.py

--- plastic_game.py
+++ plastic_game.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  5 20:33:32 2023
+
+@author: Lillemoen
+"""
+
 import json
 from typing import Union
+import numpy as np
 
 
 
@@ -20,16 +28,18 @@ class BeachMap:
 
         self.pollution_rate = 20
         self.recovery_rate = 0
+        
 
 
     def add_harvester(self, harvester_name):
-        pass
+        pass # need to increase the harvester_numbers variable by 1 at the desired index
 
 
 
 class Harvester:
-    def __init__(self, image, recovery_rate, price):
-        self.image = image
+    def __init__(self, images, recovery_rate, price, location):
+        self.images = images
+        self.image = images[0] #the image that is actually going to shown at a given time
         self.location = location
         self.recovery_rate = recovery_rate
         self.price = price
@@ -52,17 +62,26 @@ class Harvester:
         self.number += 1
         self.recovery_rate += self.recovery_rate
         
+class Seabin(Harvester):
+    def __init__(self, location):
+        # Call the constructor of the base class (Harvester)
+        super().__init__(["seabin.png"], 100, 10000, location)
+        
+    def animate(self):
+        pass #doesn't need to do anything as not animated
 
-
+class Trash_wheel(Harvester):
+    def __init__(self, location):
+        # Call the constructor of the base class (Harvester)
+        super().__init__(["trashwheel_1.png", "trashwheel_2.png", 
+                          "trashwheel_3.png", "trashwheel_4.png"], 100, 10000, location)
+        
+    def animate(self):
+        pass #doesn't need to do anything as not animated
+        
 
 
 
 def main():
 
     beach = BeachMap(POLLUTION_LEVEL)
-
-
-
-
-    
-
